e0178c4bb9aa28c2a3fc4c58ad8fbf9832a4dbf2
Daniel Urbina <edu211004@gmail.com>
2024-12-09 19:05:41 -0600

    edit finished

--- src/features/taskSlice.js
+++ src/features/taskSlice.js
@@ -1,38 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { v4 as uuid } from "uuid";
+import { createSlice } from '@reduxjs/toolkit'
+import { v4 as uuid } from 'uuid'
 
-const initialState = [{
-    id:"1",
-    title:'First task',
-    description:'First task description'
-},{
-    id:"2",
-    title:'Second task',
-    description:'Second task description'
-}]
+const initialState = [
+  {
+    id: '1',
+    title: 'First task',
+    description: 'First task description',
+  },
+  {
+    id: '2',
+    title: 'Second task',
+    description: 'Second task description',
+  },
+]
 
 const tasksSlice = createSlice({
-    name:'tasks',
-    initialState,
-    reducers:{
-        addTask: (state, action)=>{
-            state.push({
-                ...action.payload,
-                id: uuid()
-            })
-        },
-        deleteTask:(state, action)=>{
-            const deleteItem = state.find(e=> e.id=== action.payload)
-            if(deleteItem){
-                state.splice(state.indexOf(deleteItem), 1)
-            }
-        },
-        editTask: (state, action)=>{
-            console.log(action.payload)
-        }
-    }
+  name: 'tasks',
+  initialState,
+  reducers: {
+    addTask: (state, action) => {
+      state.push({
+        ...action.payload,
+        id: uuid(),
+      })
+    },
+    deleteTask: (state, action) => {
+      const deleteItem = state.find((e) => e.id === action.payload)
+      if (deleteItem) {
+        state.splice(state.indexOf(deleteItem), 1)
+      }
+    },
+    editTask: (state, action) => {
+      const { id, title, description } = action.payload
+      const foundTask = state.find((s) => s.id === id)
+      if (foundTask) {
+        foundTask.title = title
+        foundTask.description = description
+      }
+    },
+  },
 })
 
-export const {addTask, deleteTask, editTask} = tasksSlice.actions
+export const { addTask, deleteTask, editTask } = tasksSlice.actions
 
 export default tasksSlice.reducer

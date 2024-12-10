ee8cf94d8ce9fa662c14459a62673a2e1863246b
Daniel Urbina <edu211004@gmail.com>
2024-12-09 19:03:22 -0600

    preparing edit action

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router"
-import NewTaskForm from "./TaskForm"
-import TaskList from "./TaskList"
+import { BrowserRouter, Route, Routes } from 'react-router'
+import NewTaskForm from './TaskForm'
+import TaskList from './TaskList'
 
 function App() {
   return (
     <div>
       <h2>Tasks App</h2>
       <BrowserRouter>
-      <Routes>
-      <Route path="/" element={<TaskList/>} />
-      <Route path="/create-task" element={<NewTaskForm/>} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<TaskList />} />
+          <Route path="/create-task" element={<NewTaskForm />} />
+          <Route path="/edit-task/:id" element={<NewTaskForm />} />
+        </Routes>
       </BrowserRouter>
     </div>
   )
--- src/TaskForm.jsx
+++ src/TaskForm.jsx
@@ -1,39 +1,62 @@
-import { useState } from "react"
-import { useDispatch } from "react-redux"
-import { addTask } from "./features/taskSlice"
-import { useNavigate } from "react-router"
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { addTask, editTask } from './features/taskSlice'
+import { useNavigate, useParams } from 'react-router'
 
 function NewTaskForm() {
   const dispatch = useDispatch()
   const navigate = useNavigate()
+  const params = useParams()
 
+  const tasks = useSelector((state) => state.tasks)
   const [task, setTask] = useState({
-    title:'',
-    description:''
+    title: '',
+    description: '',
   })
-
-  const handleSubmit =(e)=>{
+  useEffect(() => {
+    if (params.id) {
+      setTask(tasks.find((t) => t.id === params.id))
+    }
+  }, [])
+  
+  const handleSubmit = (e) => {
     e.preventDefault()
-    dispatch(addTask(task))
-    setTask({
-      title:'',
-      description:''
-    })
+    if (params.id) {
+      dispatch(editTask(task))
+    } else {
+      dispatch(addTask(task))
+      setTask({
+        title: '',
+        description: '',
+      })
+    }
     navigate('/')
   }
-  const handleChange=(e)=>{
+  const handleChange = (e) => {
     setTask({
       ...task,
-      [e.target.name]:e.target.value
+      [e.target.name]: e.target.value,
     })
   }
   return (
     <div>
-        <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="title" name="title" onChange={handleChange} value={task.title}/>
-            <textarea name="description" id="" placeholder="description" onChange={handleChange} value={task.description}></textarea>
-            <button>Add task</button>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="title"
+          name="title"
+          onChange={handleChange}
+          value={task.title}
+        />
+        <textarea
+          name="description"
+          id=""
+          placeholder="description"
+          onChange={handleChange}
+          value={task.description}
+        ></textarea>
+        <button>Save</button>
+      </form>
     </div>
   )
 }
